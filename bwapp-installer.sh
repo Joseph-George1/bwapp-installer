fa8b05fe4df4ed71b51741001031c3e5bf18b62d
Joseph George <62404986+Joseph-George1@users.noreply.github.com>
2024-12-27 07:38:19 -0500

    Update bwapp-installer.sh

--- bwapp-installer.sh
+++ bwapp-installer.sh
@@ -2,11 +2,11 @@
 
 m_ip=$(hostname -I | awk '{print $1}')
 
-chmod +x bWAPPv2.2.zip
+chmod +x bWAPP2.2.zip
 
 sudo cp -r bWAPPv2.2.zip /var/www/html
 
-echo "sudo chmod +x bWAPP2.2.zip && sudo unzip bWAPPv2.2.zip -d /var/www/ && sudo chmod 777 /var/www/passwords/ /var/www/images/ /var/www/documents/ /var/www/logs/" > /var/www/html/install.sh
+sudo echo "wget http://${m_ip}/bWAPPv2.2.zip && sudo chmod +x bWAPP2.2.zip && sudo unzip bWAPPv2.2.zip -d /var/www/ && sudo chmod 777 /var/www/passwords/ /var/www/images/ /var/www/documents/ /var/www/logs/" > /var/www/html/install.sh
 
 sudo chmod 755 /var/www/html/install.sh
 
@@ -14,4 +14,4 @@ sudo systemctl start apache2
 
 echo "Please copy the next command and paste it in the metasbloitable2 command line"
 
-echo "wget http://${m_ip}/bWAPPv2.2.zip && wget http://${m_ip}/install.sh && ./install.sh"
+echo "wget http://${m_ip}/install.sh &&  ./install.sh"
